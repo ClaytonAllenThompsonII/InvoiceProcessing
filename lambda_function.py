6c9f60cb6ef063ee5636698099699d83ff949a5c
CTII <clay.thompson13@gmail.com>
2023-09-30 00:53:59 -0400

    🔧 Add logger and move Textract client
    - Import logging
    - Add result, extracted data.

---
+++ lambda_function.py
@@ -0,0 +1,46 @@
+"""
+Purpose
+An AWS Lambda function that analyzes invoices with Amazon Textract
+"""
+
+import base64
+import logging
+import json
+import boto3
+
+from botocore.exceptions import ClientError
+
+#Set up logging
+logger = logging.getLogger(__name__
+                           )
+#Initialize the Textract client.
+textract_client = boto3.client('textract')
+
+
+def lambda_handler(event, context):
+   
+
+    # Get s3 Bucket and object information from the trigger event
+    s3_bucket = event['Records'][0]['s3']['bucket']['name']
+    s3_key = event['Records'][0]['s3']['object']['key']
+
+    # Specify the S3 bucket where you want to store Textract output
+    output_bucket = 'your-output-bucket'
+
+    # Call Textract to process the PDF
+    response = textract_client.start_document_text_detection(
+        DocumentLocation={'S3Object': {'Bucket': s3_bucket, 'Name': s3_key}}
+    )
+
+    # Get the Job ID from the Textract response
+    job_id = response['JobId']
+
+
+    # Wait for Textract job to complete
+    textract_client.get_waiter('document_text_detection_completed').wait(JobId=job_id)
+
+    # Retrieve the Textract results
+    result = textract_client.get_document_text_detection(JobId=job_id)
+
+    # Extract structured data from Textract response (modify as needed)
+    extracted_data = extract_data(result)
