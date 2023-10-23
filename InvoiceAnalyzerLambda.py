a103136d364110917bbc9fb0dcbea234942484da
CTII <clay.thompson13@gmail.com>
2023-10-22 22:18:26 -0400

    🌐🔧 Add basic exception handling to InvoiceProcessing
    - error handling is implemented using try-except blocks. 
    - First except to catch AWS service-specific errors. + logging. 
    - 2nd except is a catch-all for unexpected errors + logging.

--- InvoiceAnalyzerLambda.py
+++ InvoiceAnalyzerLambda.py
@@ -7,16 +7,14 @@ from PIL import Image, ImageDraw
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
 # Specify S3 bucket names
 Original_bucket = 'invoice-kxseafood-591543'
 Json_output_bucket = 'extract-response-output-v1-us-east-1' # bucket for Textract invoice JSON data. 
 Modified_image_bucket = 'invoice-boxes' # create and name bucket (unless this is handled elsewhere)
 
-# # Specify the original document name
-document = '2022 0124 KX 591543 $1190.52.pdf'
-# Specify a name for the modified document with bounding boxes
-modified_document = f'{document}_with_bounding_boxes'
+# # Specify the original document name and modified document with bounding boxes
+original_document = '2022 0124 KX 591543 $1190.52.pdf'
+modified_document = f'{original_document}_with_bounding_boxes'
 
 
 
@@ -53,59 +51,71 @@ def process_invoice(event, context):
     s3_client = boto3.client('s3')
     textract_client = boto3.client('textract')
 
-    # Retrieve S3 bucket and object key from Lambda event
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    document = event['Records'][0]['s3']['object']['key']
+    try:
 
-    # Step 1: Analyze the original invoice using Amazon Textract
-    # Analyze_expense is a Textract API function that is used to analyze an invoice. 
-    response = textract_client.analyze_expense(             
-        Document={'S3Object': {'Bucket': Original_bucket, 'Name': document}} # Document is a dictionary that describes the source document Specified as dictionary with ket 'S3Object', which indicates that the source document is tored in S3. 
-    )
+        # Retrieve S3 bucket and object key from Lambda event
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        document = event['Records'][0]['s3']['object']['key']
 
-    # Log info about the analysis
-    logger.info("Analyzed invoice: {}".format(document))
+        # Step 1: Analyze the original invoice using Amazon Textract
+        # Analyze_expense is a Textract API function that is used to analyze an invoice. 
+        response = textract_client.analyze_expense(             
+            Document={'S3Object': {'Bucket': Original_bucket, 'Name': original_document}} # Document is a dictionary that describes the source document Specified as dictionary with ket 'S3Object', which indicates that the source document is tored in S3. 
+        )
 
-    # Step 2: Store the JSON output in the "textract-invoice-data" bucket
-    json_output = response  # Assign the response as json_output with the actual JSON data
-    s3_client.put_object(
+        # Log info about the analysis
+        logger.info("Analyzed invoice: {}".format(original_document))
+
+        # Step 2: Store the JSON output in the "textract-invoice-data" bucket
+        json_output = response  # Assign the response as json_output with the actual JSON data
+        s3_client.put_object(
         Bucket= Json_output_bucket,
-        Key=f'{document}.json', # name of the JSON output file will be "2022 0124 KX 591543 $1190.52.pdf.json."
+        Key=f'{original_document}.json', # name of the JSON output file will be "2022 0124 KX 591543 $1190.52.pdf.json."
         Body=json_output
-    )
+        )
 
-    # Log info about storing JSON data
-    logger.info("Stored JSON data in 'textract-invoice-data' bucket")
+        # Log info about storing JSON data
+        logger.info("Stored JSON data in 'textract-invoice-data' bucket")
 
-    # Step 3: Create a new version of the invoice with bounding boxes
-    s3_object = s3_client.Object(bucket, document)
-    s3_response = s3_object.get()
-    stream = io.BytesIO(s3_response['Body'].read())
-    image = Image.open(stream)
-    width, height = image.size
-    draw = ImageDraw.Draw(image)
+        # Step 3: Create a new version of the invoice with bounding boxes
+        s3_object = s3_client.Object(bucket, document)
+        s3_response = s3_object.get()
+        stream = io.BytesIO(s3_response['Body'].read())
+        image = Image.open(stream)
+        width, height = image.size
+        draw = ImageDraw.Draw(image)
 
-    # Iterate through the response and draw bounding boxes
-    for expense_doc in response["ExpenseDocuments"]:
-        for line_item_group in expense_doc["LineItemGroups"]:
-            for line_items in line_item_group["LineItems"]:
-                for expense_fields in line_items["LineItemExpenseFields"]:
-                    for key, val in expense_fields["ValueDetection"].items():
-                        if "Geometry" in key:
-                            draw_bounding_box(key, val, width, height, draw)
+        # Iterate through the response and draw bounding boxes
+        for expense_doc in response["ExpenseDocuments"]:
+            for line_item_group in expense_doc["LineItemGroups"]:
+                for line_items in line_item_group["LineItems"]:
+                    for expense_fields in line_items["LineItemExpenseFields"]:
+                        for key, val in expense_fields["ValueDetection"].items():
+                            if "Geometry" in key:
+                                draw_bounding_box(key, val, width, height, draw)
 
-        for label in expense_doc["SummaryFields"]:
-            if "LabelDetection" in label:
-                for key, val in label["LabelDetection"].items():
-                    draw_bounding_box(key, val, width, height, draw)
+            for label in expense_doc["SummaryFields"]:
+                if "LabelDetection" in label:
+                    for key, val in label["LabelDetection"].items():
+                        draw_bounding_box(key, val, width, height, draw)
 
-    # Save the modified image with bounding boxes
-    modified_image_path = f'/tmp/{modified_document}'
-    image.save(modified_image_path)
-    s3_client.upload_file(modified_image_path, 'invoice-boxes', modified_document)
+        # Save the modified image with bounding boxes
+        modified_image_path = f'/tmp/{modified_document}'
+        image.save(modified_image_path)
+        s3_client.upload_file(modified_image_path, 'invoice-boxes', modified_document)
 
-    # Log info about creating the modified invoice
-    logger.info("Created modified invoice with bounding boxes")
+        # Log info about creating the modified invoice
+        logger.info("Created modified invoice with bounding boxes")
+
+    # used to catch AWS service-specific errors.
+    except botocore.exceptions.ClientError as e:
+        # Handle specific AWS service errors here
+        logger.error("Error during processing: {}".format(e))
+    # catch-all for unexpected errors, allowing you to log and handle any other exceptions that may occur.
+    except Exception as e:
+        # Handle any other unexpected errors here
+        logger.error("Unexpected error: {}".format(e))
+        
 
 def lambda_handler(event, context):
     process_invoice(event, context)
